21172937728f501519131a52ae48487ed4c1f184
Ishanvi04 <Ishanvi2singh@gmail.com>
2025-07-08 21:01:25 +0530

    completed quest 1 - Reverse a String ( 2 methods)

--- 01-reverse-string/solution.js
+++ 01-reverse-string/solution.js
@@ -1,4 +1,16 @@
-function reverseString(str){
+// Method 1: Using built-in functions
+function reverseString(str) {
     return str.split('').reverse().join('');
-}
-console.log(reverseString("I am the Best"));
+  }
+  console.log(ReverseString("I am the Best"));
+  
+  // Method 2: without using built-in functions
+  function reverseaString(str) {
+    let reverse = ""; //starting with an empty string for stpring the reversed
+    for (let i = str.length - 1; i >= 0; i--) { //starting from the last character
+      reverse += str[i];//Adding each character to the reverse string one by one from the end
+    }
+    return reverse;//final result
+  }
+  console.log( reverseaString("I am the Best"));
+  
